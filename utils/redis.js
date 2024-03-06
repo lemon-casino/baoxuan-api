342cba312494161396e852767c4aa8beeae43a11
娄理想 <1875483499@qq.com>
2024-03-06 11:14:19 +0800

    判断是否需要密码创建redisClient、使用setEx替换set和expire操作

--- utils/redis.js
+++ utils/redis.js
@@ -10,60 +10,52 @@ const redis = require("redis");
 const redisConfig = require("../config/index").redisConfig;
 // 创建客户端实例的方式已经改变
 // 现在使用 createClient 方法接受一个对象参数
-const client = redis.createClient({
-  // 注意：port 和 host 参数应该放在一个对象中传递
-  // 如果你的 Redis 服务器运行在标准端口（6379）上，并且是本地的，这些参数可以省略
-  url: `redis://${redisConfig.url}:${redisConfig.port}`,
-  // 如果 Redis 服务器需要密码，可以这样传递
-  // password: config.password,
-});
-(async () => { await client.connect() })();
+const options = {
+    url: `redis://${redisConfig.url}:${redisConfig.port}`,
+}
+if (redisConfig.password) {
+    options["password"] = redisConfig.password
+}
+client = redis.createClient(options);
 
+(async () => {
+    await client.connect()
+})();
 
 client.on("error", (err) => {
-  console.log("Redis 失败日志：" + err);
+    console.log("Redis 失败日志：" + err);
 });
 
 client.on("connect", () => {
-  console.log("Redis 已启动");
+    console.log("Redis 已启动");
 });
-// 在 Redis 4.x 中，不需要显式调用 auth 方法
-// 如果提供了 password 配置，客户端会自动尝试认证
 
 // 连接到 Redis
 const redisConnect = {};
 // 延迟函数
 const delay = (ms = 800) => new Promise((res) => setTimeout(res, ms));
 redisConnect.setKey = async (key, value, expire) => {
-  try {
-    // await client.connect();
-    let reply;
-    // 检查 expire 是否为有效数值，如果是，则使用 'EX' 选项设置过期时间
-    if (!isNaN(expire) && expire > 0) {
-      console.log("加时间=========>");
-      reply = await client.set(key, value);
-      await client.expire(key, expire);
-    } else {
-      console.log("不加时间=========>");
-      // 如果没有提供有效的 expire，只设置键值对，不设置过期时间
-      reply = await client.set(key, value);
+    try {
+        // 检查 expire 是否为有效数值，如果是，则使用 'EX' 选项设置过期时间
+        if (!isNaN(expire) && expire > 0) {
+            await client.setEx(key, expire, value);
+        } else {
+            // 如果没有提供有效的 expire，只设置键值对，不设置过期时间
+            await client.set(key, value);
+        }
+        return value;
+    } catch (err) {
+        console.error("redis 存储出错了======》", err);
+        throw err;
     }
-    return reply;
-  } catch (err) {
-    console.error("redis 存储出错了======》", err);
-    throw err;
-  }
 };
 
-
 redisConnect.getKey = async (key) => {
-  try {
-    let reply;
-    reply = await client.get(key);
-    return reply;
-  } catch (error) {
-  }
-  return;
+    try {
+        return await client.get(key);
+    } catch (error) {
+    }
+    return null;
 };
 
 module.exports = redisConnect;
