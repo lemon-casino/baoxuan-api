b3dd4c539dfad672ad4f440a499d51dafed549be
娄理想 <1875483499@qq.com>
2024-03-26 12:55:12 +0800

    修改日志信息

--- utils/log.js
+++ utils/log.js
@@ -23,16 +23,6 @@ const logger = createLogger({
     defaultMeta: {service: "bi"},
     transports: [
         new DailyRotateFile({
-            filename: path.join(`${logDirectory}/%DATE%`, `debug.log`),
-            datePattern: 'YYYY-MM-DD',
-            level: process.env.NODE_ENV === "dev" ? "debug" : ""
-        }),
-        new DailyRotateFile({
-            filename: path.join(`${logDirectory}/%DATE%`, `info.log`),
-            datePattern: 'YYYY-MM-DD',
-            level: "info"
-        }),
-        new DailyRotateFile({
             filename: path.join(`${logDirectory}/%DATE%`, `warm.log`),
             datePattern: 'YYYY-MM-DD',
             level: "warm"
