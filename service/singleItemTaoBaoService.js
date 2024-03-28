143807f2375c1487dcff383f81f46fddda40c46b
娄理想 <1875483499@qq.com>
2024-03-28 09:10:56 +0800

    增加数字的toString()

--- service/singleItemTaoBaoService.js
+++ service/singleItemTaoBaoService.js
@@ -48,7 +48,7 @@ const saveSingleItemTaoBao = async (item) => {
         if (!value) {
             continue
         }
-        if (value.indexOf("%")) {
+        if (value.toString().indexOf("%")) {
             value = value.replace("%", "").trim()
             if (/^[0-9]+\.*[0-9]+$/.test(value)) {
                 value = parseFloat(value)
