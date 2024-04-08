aaedbfeb87d4905fee35ec1ba9d9ff30550a8847
娄理想 <1875483499@qq.com>
2024-04-08 10:10:30 +0800

    临时保存

---
+++ const/joiConst.js
@@ -0,0 +1,19 @@
+const Joi = require("joi")
+
+const joiErrorMessages = {
+    "any.required": "不能为空",
+    "any.empty": "不能为空",
+    "date.base": "日期格式不正确"
+}
+
+const commonJoiSchemas = {
+    pageIndex: Joi.string().alphanum().required(),
+    pageSize: Joi.string().alphanum().required(),
+    startDate: Joi.date().required(),
+    endDate: Joi.date().required()
+}
+
+module.exports = {
+    joiErrorMessages,
+    commonJoiSchemas
+}
