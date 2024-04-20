e576ef56bed79f04f2384212420b0cf59e0e95dc
娄理想 <1875483499@qq.com>
2024-04-20 12:40:41 +0800

    线上

--- const/tmp/formImportantItems.js
+++ const/tmp/formImportantItems.js
@@ -1,5 +1,6 @@
 module.exports = [
     {
-        formId: "FORM-027086325D5B4B24885D75A541FD633E7AMV", formName: "天猫链接上架流程", items: ["1",2,3]
+        formId: "FORM-0X966971LL0EI3OC9EJWUATDC84838H8V09ML1", formName: "天猫链接上架流程",
+        items: ["radioField_lv6jkbdk", "radioField_lv6hlshu", "radioField_lv7ht3fd"]
     }
 ]
