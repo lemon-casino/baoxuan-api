49344fe99f8a88a990f0eb6ce2f92ce172a1aa19
娄理想 <1875483499@qq.com>
2024-04-22 12:43:10 +0800

    tmp

--- const/tmp/coreReviewItemsConst.js
+++ const/tmp/coreReviewItemsConst.js
@@ -16,12 +16,26 @@ const tmCoreReviewItems = [{
             flowFormId: "FORM-33666CB1FV8BQCCE9IWPV4DYQIEJ34M5Q9IILP", flowFormName: "采购选品会",
             coreReviewItems: [{nodeId: "node_ocliieab3b1s", title: "运营提交市场分析方案"}]
         },
-        {flowFormId: "", flowFormName: "天猫链接打仗审核流程",
+        {
+            flowFormId: "FORM-495A1584CBE84928BB3B1E0D4AA4B56AYN1J", flowFormName: "天猫链接打仗审核流程",
             coreReviewItems: [
-                {nodeId: "", title: ""}
-            ]},
-        {flowFormId: "", flowFormName: "运营优化方案", coreReviewItems: []},
-        {flowFormId: "", flowFormName: "开发新品设计流程", coreReviewItems: []},
+                {nodeId: "node_oclqhp9c103", title: "提交优化方案"}
+            ]
+        },
+        {
+            flowFormId: "FORM-CP766081CPAB676X6KT35742KAC229LLKHIILB", flowFormName: "运营优化方案",
+            coreReviewItems: [{nodeId: "node_oclu3u9gmm1", title: "制定优化方案"}]
+        },
+        {
+            flowFormId: "FORM-2529762FC54F44849153E5564C1628FAHFKN", flowFormName: "开发新品设计流程",
+            coreReviewItems: [
+                // todo: 还缺少好几个运营提交营销方案，等表单流程入库后补充
+                {nodeId: "node_ocluv2dys83", title: "运营提交营销方案"},
+                {nodeId: "node_ocluv3sh959", title: "运营提交营销方案"},
+                // todo: 库中暂时没有等表单流程入库后补充
+                // {nodeId: "", title: "运营提交营销方案"}
+            ]
+        },
     ]
 }]
 
