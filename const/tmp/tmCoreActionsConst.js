8a47c8ac6717d01556f87ac7786644754e2ca33d
娄理想 <1875483499@qq.com>
2024-04-30 10:20:47 +0800

    天猫核心流程配置数据初步整理

--- const/tmp/tmCoreActionsConst.js
+++ const/tmp/tmCoreActionsConst.js
@@ -1,30 +1,31 @@
 const tmCoreActions = [{
     actionName: "爆款方案",
-    statuses: [
+    actionStatus: [
         {
-            name: "待做",
-            forms: [{
-                formName: "运营新品流程",
-                formId: "FORM-6L966171SX9B1OIODYR0ICISRNJ13A9F75IIL3",
-                startNodes: [{
-                    id: "",
-                    name: "提交竞店ID",
-                    statuses: ["TODO", "HISTORY"]
-                }],
-                endNodes: [{
-                    id: "node_oclii6vcap1",
-                    name: "五维表分析",
-                    statuses: ["TODO", "FORECAST"]
-                }],
-                overDueNode: {
-                    id: "",
-                    name: "审核执行统计五维表"
+            statusName: "待做",
+            forms: [
+                {
+                    formName: "运营新品流程",
+                    formId: "FORM-6L966171SX9B1OIODYR0ICISRNJ13A9F75IIL3",
+                    startNodes: [{
+                        nodeId: "",
+                        nodeName: "提交竞店ID",
+                        nodeStatus: ["TODO", "HISTORY"]
+                    }],
+                    endNodes: [{
+                        nodeId: "node_oclix34wly1",
+                        nodeName: "审核执行统计五维表",
+                        nodeStatus: ["TODO", "FORECAST"]
+                    }],
+                    overDueNode: {
+                        nodeId: "node_oclix34wly1",
+                        nodeName: "审核执行统计五维表"
+                    },
+                    personInCharge: {
+                        from: "form",
+                        id: "employeeField_lii5gvq3"
+                    }
                 },
-                personInCharge: {
-                    from: "process",
-                    id: "node_oclimxk5wu1" // ？
-                }
-            },
                 {
                     name: "宝可梦新品开发流程",
                     formId: "FORM-CC0B476071F24581B129A24835910B81AK56",
@@ -36,20 +37,11 @@ const tmCoreActions = [{
                     // 分支情况
                     endNodes: [
                         {
-                            id: "node_oclv1zonogt",
-                            name: "运营提交营销方案",
+                            id: "node_oclvkcjxy42",
+                            name: "执行提交市场分析方案",
                             statuses: ["TODO", "FORECAST"]
-                        },
-                        {
-                            id: "node_oclv1zonogv",
-                            name: "运营提交营销方案",
-                            statuses: ["TODO", "FORECAST"]
-                        },
-                        {
-                            id: "node_oclv1zonogw",
-                            name: "运营提交营销方案",
-                            statuses: ["TODO", "FORECAST"]
-                        }],
+                        }
+                    ],
                     overDueNode: {
                         id: "node_oclvkcjxy41",
                         name: "分配执行营销分析方案"
@@ -87,70 +79,373 @@ const tmCoreActions = [{
         {
             name: "在做",
             forms: [{
-                formName: "",
-                formId: "",
-                id: "",
-                startNode: {
-                    id: "",
-                    status: []
+                formName: "运营新品流程",
+                formId: "FORM-6L966171SX9B1OIODYR0ICISRNJ13A9F75IIL3",
+                node: {
+                    id: "node_oclii6vcap1",
+                    name: "五维表分析",
+                    status: ["TODO"]
                 },
-                endNode: {
-                    id: "",
-                    statuses: []
+                personInCharge: {
+                    from: "form",
+                    id: "employeeField_lii5gvq3"
                 }
-            }]
+            },
+                {
+                    formName: "宝可梦新品开发流程",
+                    formId: "FORM-CC0B476071F24581B129A24835910B81AK56",
+                    node: {
+                        id: "node_oclv1zonogt",
+                        name: "运营提交营销方案",
+                        status: ["TODO"]
+                    },
+                    personInCharge: {
+                        from: "form",
+                        id: "employeeField_luv1lfuq"
+                    }
+                },
+                {
+                    formName: "天猫链接上架流程",
+                    formId: "FORM-0X966971LL0EI3OC9EJWUATDC84838H8V09ML1",
+                    node: {
+                        id: "node_oclo8dzm951",
+                        name: "提交五维表方案",
+                        status: ["TODO"]
+                    },
+                    personInCharge: {
+                        from: "process",
+                        name: "发起",
+                        id: "node_ockpz6phx72"
+                    }
+                }]
         },
         {
             name: "已做",
             forms: [{
-                startNode: {
-                    id: "",
-                    status: []
+                formName: "运营新品流程",
+                formId: "FORM-6L966171SX9B1OIODYR0ICISRNJ13A9F75IIL3",
+                node: {
+                    id: "node_oclii6vcap1",
+                    name: "五维表分析",
+                    status: ["HISTORY"]
                 },
-                endNode: {
-                    id: "",
-                    statuses: []
+                personInCharge: {
+                    from: "form",
+                    id: "employeeField_lii5gvq3"
                 }
-            }]
-        }
-    ],
-
-    status1: [
-        {
-            flowFormId: "FORM-6L966171SX9B1OIODYR0ICISRNJ13A9F75IIL3",
-            flowFormName: "运营新品流程",
-            coreReviewItems: [{nodeId: "node_oclii6vcap1", title: "五维表分析"}]
-        },
-        {
-            flowFormId: "FORM-0X966971LL0EI3OC9EJWUATDC84838H8V09ML1",
-            flowFormName: "天猫链接上架流程",
-            coreReviewItems: [{nodeId: "node_oclo8dzm951", title: "提交五维表方案"}]
-        },
-        {
-            flowFormId: "FORM-33666CB1FV8BQCCE9IWPV4DYQIEJ34M5Q9IILP", flowFormName: "采购选品会",
-            coreReviewItems: [{nodeId: "node_ocliieab3b1s", title: "运营提交市场分析方案"}]
-        },
-        {
-            flowFormId: "FORM-495A1584CBE84928BB3B1E0D4AA4B56AYN1J", flowFormName: "天猫链接打仗审核流程",
-            coreReviewItems: [
-                {nodeId: "node_oclqhp9c103", title: "提交优化方案"}
-            ]
-        },
-        {
-            flowFormId: "FORM-CP766081CPAB676X6KT35742KAC229LLKHIILB", flowFormName: "运营优化方案",
-            coreReviewItems: [{nodeId: "node_oclu3u9gmm1", title: "制定优化方案"}]
-        },
-        {
-            flowFormId: "FORM-2529762FC54F44849153E5564C1628FAHFKN", flowFormName: "开发新品设计流程",
-            coreReviewItems: [
-                // todo: 还缺少好几个运营提交营销方案，等表单流程入库后补充
-                {nodeId: "node_ocluv2dys83", title: "运营提交营销方案"},
-                {nodeId: "node_ocluv3sh959", title: "运营提交营销方案"},
-                // todo: 库中暂时没有等表单流程入库后补充
-                // {nodeId: "", title: "运营提交营销方案"}
-            ]
+            },
+                {
+                    formName: "宝可梦新品开发流程",
+                    formId: "FORM-CC0B476071F24581B129A24835910B81AK56",
+                    node: {
+                        id: "node_oclv1zonogt",
+                        name: "运营提交营销方案",
+                        status: ["HISTORY"]
+                    },
+                    personInCharge: {
+                        from: "form",
+                        id: "employeeField_luv1lfuq"
+                    }
+                },
+                {
+                    formName: "天猫链接上架流程",
+                    formId: "FORM-0X966971LL0EI3OC9EJWUATDC84838H8V09ML1",
+                    node: {
+                        id: "node_oclo8dzm951",
+                        name: "提交五维表方案",
+                        status: ["HISTORY"]
+                    },
+                    personInCharge: {
+                        from: "process",
+                        name: "发起",
+                        id: "node_ockpz6phx72"
+                    }
+                }]
         },
     ]
-}]
+}, {
+    actionName: "市场分析",
+    statuses: [
+        {
+            name: "待做",
+            forms: [
+                {
+                    formName: "宝可梦新品开发流程",
+                    formId: "FORM-CC0B476071F24581B129A24835910B81AK56",
+                    startNodes: [{
+                        id: "node_oclv1xr53i4",
+                        name: "分配执行市场分析方案",
+                        statuses: ["TODO", "HISTORY"]
+                    }],
+                    // 分支情况
+                    endNodes: [
+                        {
+                            id: "node_oclvkc9r0a3",
+                            name: "审核执行市场分析方案",
+                            statuses: ["TODO", "FORECAST"]
+                        }
+                    ],
+                    overDueNode: {
+                        id: "node_oclvkc9r0a3",
+                        name: "审核执行市场分析方案"
+                    },
+                    personInCharge: {
+                        from: "form",
+                        name: "分配运营做市场分析",
+                        id: "employeeField_luv1lfuq"
+                    }
+                },
+                {
+                    formName: "采购任务运营发布",
+                    formId: "FORM-0A966I819O8BZMVBE16JLAK96KK42KD1QEIILC",
+                    startNodes: [{
+                        id: "node_ockpz6phx72",
+                        name: "发起",
+                        statuses: ["TODO", "HISTORY"]
+                    }],
+                    endNodes: [
+                        {
+                            id: "node_oclvesclp04",
+                            name: "运营提交市场分析",
+                            statuses: ["TODO", 'FORECAST']
+                        },
+                        {
+                            id: "node_oclvesclp09",
+                            name: "运营提交市场分析",
+                            statuses: ["TODO", 'FORECAST']
+                        },
+                        {
+                            id: "node_oclvesclp0d",
+                            name: "运营提交市场分析",
+                            statuses: ["TODO", 'FORECAST']
+                        }],
+                    overDueNode: {},
+                    personInCharge: {
+                        from: "process",
+                        name: "发起人",
+                        id: "node_ockpz6phx72"
+                    }
+                },
+                // 仅天猫
+                {
+                    formName: "采购选品会",
+                    formId: "FORM-33666CB1FV8BQCCE9IWPV4DYQIEJ34M5Q9IILP",
+                    startNodes: [{
+                        id: "node_oclklxv8kk1",
+                        name: "运营负责人提交市场统计模板",
+                        statuses: ["TODO", "HISTORY"]
+                    }],
+                    endNodes: [{
+                        id: "node_oclix0mfzn1",
+                        name: "执行提交市场分析表",
+                        statuses: ["TODO", "HISTORY"]
+                    }],
+                    overDueNode: {
+                        id: "node_oclklxv8kk1",
+                        name: "运营负责人提交市场统计模板"
+                    },
+                    personInCharge: {
+                        from: "form",
+                        name: "运营负责人",
+                        id: "employeeField_lii9qts2"
+                    }
+                }]
+        },
+        {
+            name: "在做",
+            forms: [{
+                formName: "宝可梦新品开发流程",
+                formId: "FORM-CC0B476071F24581B129A24835910B81AK56",
+                node: {
+                    id: "node_oclvkc9r0a4",
+                    name: "完成市场分析方案",
+                    status: ["TODO"]
+                },
+                personInCharge: {
+                    from: "form",
+                    id: "employeeField_luv1lfuq"
+                }
+            },
+                {
+                    formName: "采购任务运营发布",
+                    formId: "FORM-0A966I819O8BZMVBE16JLAK96KK42KD1QEIILC",
+                    nodes: [{
+                        id: "node_oclvesclp04",
+                        name: "运营提交市场分析",
+                        status: ["TODO"]
+                    }, {
+                        id: "node_oclvesclp09",
+                        name: "运营提交市场分析",
+                        status: ["TODO"]
+                    }, {
+                        id: "node_oclvesclp0d",
+                        name: "运营提交市场分析",
+                        status: ["TODO"]
+                    },],
+                    personInCharge: {
+                        from: "process",
+                        id: "node_ockpz6phx72"
+                    }
+                },
+                {
+                    formName: "采购选品会",
+                    formId: "FORM-33666CB1FV8BQCCE9IWPV4DYQIEJ34M5Q9IILP",
+                    node: {
+                        id: "node_ocliieab3b1",
+                        name: "运营提交市场分析方案",
+                        status: ["TODO"]
+                    },
+                    personInCharge: {
+                        from: "form",
+                        name: "运营负责人",
+                        id: "employeeField_lii9qts2"
+                    }
+                }]
+        },
+        {
+            name: "已做",
+            forms: [{
+                formName: "宝可梦新品开发流程",
+                formId: "FORM-CC0B476071F24581B129A24835910B81AK56",
+                node: {
+                    id: "node_oclvkc9r0a4",
+                    name: "完成市场分析方案",
+                    status: ["HISTORY"]
+                },
+                personInCharge: {
+                    from: "form",
+                    id: "employeeField_luv1lfuq"
+                }
+            },
+                {
+                    formName: "采购任务运营发布",
+                    formId: "FORM-0A966I819O8BZMVBE16JLAK96KK42KD1QEIILC",
+                    nodes: [{
+                        id: "node_oclvesclp04",
+                        name: "运营提交市场分析",
+                        status: ["HISTORY"]
+                    }, {
+                        id: "node_oclvesclp09",
+                        name: "运营提交市场分析",
+                        status: ["HISTORY"]
+                    }, {
+                        id: "node_oclvesclp0d",
+                        name: "运营提交市场分析",
+                        status: ["HISTORY"]
+                    },],
+                    personInCharge: {
+                        from: "process",
+                        id: "node_ockpz6phx72"
+                    }
+                },
+                {
+                    formName: "采购选品会",
+                    formId: "FORM-33666CB1FV8BQCCE9IWPV4DYQIEJ34M5Q9IILP",
+                    node: {
+                        id: "node_ocliieab3b1",
+                        name: "运营提交市场分析方案",
+                        status: ["HISTORY"]
+                    },
+                    personInCharge: {
+                        from: "form",
+                        name: "运营负责人",
+                        id: "employeeField_lii9qts2"
+                    }
+                }]
+        },
+    ]
+}, {
+    actionName: "优化方案",
+    statuses: [
+        {
+            name: "待做",
+            forms: [
+                {
+                    name: "天猫链接打仗审核流程",
+                    formId: "",
+                    startNodes: [{
+                        id: "node_ockpz6phx72",
+                        name: "发起",
+                        statuses: ["TODO", "HISTORY"]
+                    }],
+                    // 分支情况
+                    endNodes: [
+                        {
+                            id: "node_oclqhp9c102",
+                            name: "执行BI系统打标签",
+                            statuses: ["TODO", "FORECAST"]
+                        }
+                    ],
+                    personInCharge: {
+                        from: "process",
+                        name: "发起人",
+                        id: "node_ockpz6phx72"
+                    }
+                }]
+        },
+        {
+            name: "在做",
+            forms: [{
+                formName: "天猫链接打仗审核流程",
+                formId: "",
+                node: {
+                    id: "node_oclqhp9c103",
+                    name: "提交优化方案",
+                    status: ["TODO"]
+                },
+                personInCharge: {
+                    from: "process",
+                    name: "发起人",
+                    id: "node_ockpz6phx72"
+                }
+            },
+                {
+                    formName: "运营优化方案流程",
+                    formId: "FORM-CP766081CPAB676X6KT35742KAC229LLKHIILB",
+                    node: {
+                        id: "node_oclu3u9gmm1",
+                        name: "制定优化方案",
+                        status: ["TODO"]
+                    },
+                    personInCharge: {
+                        from: "form",
+                        name: "运营负责人",
+                        id: "employeeField_liihs7l0"
+                    }
+                }]
+        },
+        {
+            name: "已做",
+            forms: [{
+                formName: "天猫链接打仗审核流程",
+                formId: "FORM-495A1584CBE84928BB3B1E0D4AA4B56AYN1J",
+                node: {
+                    id: "node_oclqhp9c103",
+                    name: "提交优化方案",
+                    status: ["HISTORY"]
+                },
+                personInCharge: {
+                    from: "process",
+                    name: "发起人",
+                    id: "node_ockpz6phx72"
+                }
+            },
+                {
+                    formName: "运营优化方案流程",
+                    formId: "FORM-CP766081CPAB676X6KT35742KAC229LLKHIILB",
+                    node: {
+                        id: "node_oclu3u9gmm1",
+                        name: "制定优化方案",
+                        status: ["HISTORY"]
+                    },
+                    personInCharge: {
+                        from: "form",
+                        name: "运营负责人",
+                        id: "employeeField_liihs7l0"
+                    }
+                }]
+        },
+    ]
+},]
 
 module.exports = {}
