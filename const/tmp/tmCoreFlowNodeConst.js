34b65165664dd692566b7f58a5c4048ec029d646
娄理想 <1875483499@qq.com>
2024-05-06 16:56:20 +0800

    整理天猫核心流程配置数据

---
+++ const/tmp/tmCoreFlowNodeConst.js
@@ -0,0 +1,444 @@
+const flows = [
+    {
+        // 采购选品会
+        formName: "采购选品会",
+        formId: "FORM-33666CB1FV8BQCCE9IWPV4DYQIEJ34M5Q9IILP",
+        actions: [
+            {
+                name: "审核产品",
+                nodeIds: [
+                    "node_oclv1t0i396",
+                    "node_oclv1t0i398",
+                    "node_oclv1t0i39a",
+                    "node_oclv24zen52",
+                    "node_oclv24zen56",
+                    "node_oclv2a2mj42",
+                    "node_oclv2a2mj45",
+                    "node_oclv2a2mj48",
+                    "node_oclv2a2mj4b"
+                ]
+            },
+            {
+                name: "分配运营分析",
+                nodeIds: [
+                    "node_oclv1t0i397",
+                    "node_oclv1t0i399",
+                    "node_oclv1t0i39b"
+                ]
+            },
+            {
+                name: "运营负责人提交市场统计模板",
+                nodeIds: [
+                    "node_oclklxv8kk1",
+                    "node_oclv1ugwu31",
+                    "node_oclv1ugwu32",
+                    "node_oclv24zen54",
+                    "node_oclv24zen58",
+                    "node_oclv2a2mj43",
+                    "node_oclv2a2mj46",
+                    "node_oclv2a2mj49",
+                    "node_oclv2a2mj4c"
+                ]
+            },
+            {
+                name: "运营提交市场分析方案",
+                nodeIds: ["node_ocliieab3b1"]
+            },
+            {
+                name: "组长审核方案",
+                nodeIds: [
+                    "node_oclv2a2mj4h",
+                    "node_oclv2a2mj4k"
+                ]
+            },
+            {
+                name: "项目负责人审核方案",
+                nodeIds: [
+                    "node_oclv2a2mj4i",
+                    "node_oclv2a2mj4l",
+                    "node_oclv2a2mj4m"
+                ]
+            },
+            {
+                name: "运营确认样品预估销量",
+                nodeIds: [
+                    "node_ocliid9xm73"
+                ]
+            },
+            {
+                name: "审核订货量",
+                nodeIds: [
+                    "node_oclm60vm9a1",
+                    "node_oclv2ayeqti",
+                    "node_oclv2ayeqtj"
+                ]
+            }
+        ]
+    },
+
+    // 运营优化方案
+    {
+        formName: "运营优化方案",
+        formId: "FORM-CP766081CPAB676X6KT35742KAC229LLKHIILB",
+        actions: [
+            {
+                name: "制定优化方案",
+                nodeIds: ["node_oclu3u9gmm1"]
+            },
+            {
+                name: "组长审核方案",
+                nodeIds: [
+                    "node_oclto07a599",
+                    "node_oclto07a59b",
+                    "node_oclv7ila163",
+                    "node_ocltnzlp8s7",
+                    "node_oclto07a593",
+                    "node_oclv7ipmzf2"
+                ]
+            },
+            {
+                name: "项目负责人审核方案",
+                nodeIds: [
+                    "node_oclv7ila164",
+                    "node_oclto07a594"
+                ]
+            },
+            {
+                name: "优化动作完成确认",
+                nodeIds: [
+                    "node_oclnpduv0g6",
+                    "node_ocljzcy54s1"
+                ]
+            }
+        ]
+    },
+
+    //
+    {
+        formName: "天猫链接打仗审核流程",
+        formId: "FORM-495A1584CBE84928BB3B1E0D4AA4B56AYN1J",
+        actions: [
+            {
+                name: "审核链接状态",
+                nodeIds: [
+                    "node_oclqhp9c101"
+                ]
+            },
+            {
+                name: "提交优化方案",
+                nodeIds: [
+                    "node_oclqhp9c103"
+                ]
+            },
+            {
+                name: "审核优化方案",
+                nodeIds: [
+                    "node_oclqhp9c104"
+                ]
+            },
+            {
+                name: "确认优化完成",
+                nodeIds: [
+                    "node_oclqhp9c105"
+                ]
+            },
+            {
+                name: "运营确认数据恢复",
+                nodeIds: [
+                    "node_ocltqyzlu32"
+                ]
+            },
+            {
+                name: "审核优化效果",
+                nodeIds: [
+                    "node_ockpz6phx73"
+                ]
+            }
+        ]
+    },
+    {
+        formName: "天猫链接上架流程",
+        formId: "FORM-0X966971LL0EI3OC9EJWUATDC84838H8V09ML1",
+        actions: [
+            {
+                name: "审核执行动作完成",
+                nodeIds: [
+                    "node_oclm91902f2"
+                ]
+            },
+            {
+                name: "提交五维表方案",
+                nodeIds: [
+                    "node_oclo8dzm951"
+                ]
+            },
+            {
+                // todo: 没有该节点
+                name: "组长审核方案",
+                nodeIds: [
+                    ""
+                ]
+            },
+            {
+                // todo: 没有该节点
+                name: "项目负责人审核方案",
+                nodeIds: []
+            },
+            {
+                name: "提交订货明细",
+                nodeIds: [
+                    "node_ocltpgxaqr5"
+                ]
+            },
+            {
+                name: "审核视觉完成",
+                nodeIds: [
+                    "node_oclm91ca7le"
+                ]
+            }
+        ]
+    },
+    //
+    {
+        formName: "运营新品流程",
+        formId: "FORM-6L966171SX9B1OIODYR0ICISRNJ13A9F75IIL3",
+        actions: [
+            {
+                name: "提交竞店ID",
+                nodeIds: [
+                    "node_oclkvznwuu1"
+                ]
+            },
+            {
+                name: "审核执行统计五维表",
+                nodeIds: [
+                    "node_oclix34wly1"
+                ]
+            },
+            {
+                name: "五维表分析",
+                nodeIds: [
+                    "node_oclii6vcap1"
+                ]
+            },
+            // todo:  无该节点
+            {
+                name: "组长审核方案",
+                nodeIds: [
+                    ""
+                ]
+            },
+            // todo:  无该节点
+            {
+                name: "项目负责人审核方案",
+                nodeIds: []
+            },
+            {
+                name: "运营提供样品明细",
+                nodeIds: [
+                    "node_oclmbqrsr56"
+                ]
+            },
+            {
+                name: "视觉完成确认审核",
+                nodeIds: [
+                    "node_ockpz6phx73"
+                ]
+            },
+            {
+                name: "审核上架完成",
+                nodeIds: [
+                    "node_oclii7t3xt6"
+                ]
+            },
+        ]
+    },
+
+    // 宝可梦新品开发流程
+    {
+        formName: "宝可梦新品开发流程",
+        formId: "FORM-CC0B476071F24581B129A24835910B81AK56",
+        actions: [
+            {
+                name: "审核产品是否做",
+                nodeIds: [
+                    "node_oclv1wec4t1",
+                    "node_oclv1wec4t2",
+                    "node_oclv1wec4t3",
+                    "node_oclv1wec4t4"
+                ]
+            },
+            {
+                name: "运营提交客群分析",
+                nodeIds: [
+                    "node_ocluts9plxb",
+                    "node_oclv1wec4te",
+                    "node_oclv212smo8"
+                ]
+            },
+            {
+                name: "分配运营做市场方案",
+                nodeIds: [
+                    "node_oclv1xgp7v8",
+                    "node_oclv1zji4a1",
+                    "node_oclv1zji4a7"
+                ]
+            },
+            {
+                name: "分配执行市场分析方案",
+                nodeIds: ["node_oclv1xr53i4"]
+            },
+            {
+                name: "审核执行市场分析方案",
+                nodeIds: ["node_oclvkc9r0a3"]
+            },
+            {
+                name: "完成市场分析方案",
+                nodeIds: [
+                    "node_oclvkc9r0a4"
+                ]
+            },
+            {
+                name: "审核运营市场分析方案",
+                nodeIds: [
+                    "node_oclv22kx374",
+                    "node_oclv22kx375"
+                ]
+            },
+            {
+                name: "分配运营做营销方案",
+                nodeIds: [
+                    "node_oclv1zonogy",
+                    "node_oclv1zonogz",
+                    "node_oclv1zonog10"
+                ]
+            },
+            {
+                name: "分配执行营销分析方案",
+                nodeIds: [
+                    "node_oclvkcjxy41"
+                ]
+            },
+            {
+                name: "运营提交营销方案",
+                nodeIds: [
+                    "node_oclv1zonogt",
+                    "node_oclv1zonogv",
+                    "node_oclv1zonogw"
+                ]
+            },
+            {
+                name: "审核营销方案",
+                nodeIds: [
+                    "node_oclv27pdv11",
+                    "node_oclv27pdv12",
+                    "node_oclv27pdv13"
+                ]
+            }
+        ]
+    },
+    {
+        formName: "开发新品设计流程",
+        formId: "FORM-2529762FC54F44849153E5564C1628FAHFKN",
+        actions: [
+            {
+                name: "审核产品是否做",
+                nodeIds: [
+                    "node_oclv1wec4t1",
+                    "node_oclv1wec4t2",
+                    "node_oclv1wec4t3",
+                    "node_oclv1wec4t4"
+                ]
+            },
+            {
+                name: "运营提交客群分析",
+                nodeIds: [
+                    "node_oclv212smo8",
+                    "node_oclv1wec4te",
+                    "node_ocluts9plxb"
+                ]
+            },
+            {
+                name: "分配运营做市场方案",
+                nodeIds: [
+                    "node_oclv1xgp7v8",
+                    "node_oclv1zji4a1",
+                    "node_oclv1zji4a7",
+                    "node_oclv28u6jsj",
+                    "node_oclv28u6jsp",
+                    "node_oclv28u6jsy"
+                ]
+            },
+            {
+                name: "分配执行做市场分析",
+                nodeIds: [
+                    "node_oclvkcrnz61"
+                ]
+            },
+            {
+                name: "运营提交市场分析方案",
+                nodeIds: [
+                    "node_oclv1xr53i4",
+                    "node_oclv1zji4a5",
+                    "node_oclv1zji4ab",
+                    "node_oclv1zonog9",
+                    "node_oclv28u6jsn",
+                    "node_oclv28u6jsw",
+                    "node_oclv28u6js12",
+                    "node_oclv28u6js13"
+                ]
+            },
+            {
+                name: "审核运营市场分析方案",
+                nodeIds: [
+                    "node_oclv22kx374",
+                    "node_oclv22kx375",
+                    "node_oclv28u6jso",
+                    "node_oclv28u6jsx",
+                    "node_oclv22kx376"
+                ]
+            },
+            {
+                name: "分配运营做营销方案",
+                nodeIds: [
+                    "node_oclv1zonogy",
+                    "node_oclv1zonogz",
+                    "node_oclv1zonog10",
+                    "node_oclv28u6js1b",
+                    "node_oclv28u6js1e",
+                    "node_oclv28u6js1h"
+                ]
+            },
+            {
+                name: "分配执行做营销分析",
+                nodeIds: [
+                    "node_oclvkcrnz63"
+                ]
+            },
+            {
+                name: "运营提交营销方案",
+                nodeIds: [
+                    "node_oclv1zonogt",
+                    "node_oclv1zonogv",
+                    "node_oclv1zonogw",
+                    "node_oclv28u6js1c",
+                    "node_oclv28u6js1f",
+                    "node_oclv28u6js1i"
+                ]
+            },
+            {
+                name: "审核营销方案",
+                nodeIds: [
+                    "node_oclv28u6js1j",
+                    "node_oclv28u6js1g",
+                    "node_oclv28u6js1d",
+                    "node_oclv27pdv13",
+                    "node_oclv27pdv12",
+                    "node_oclv27pdv11"
+                ]
+            }
+        ]
+    }
+]
+
+module.exports = {}
