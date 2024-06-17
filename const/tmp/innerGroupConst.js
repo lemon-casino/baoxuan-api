dddce599dc6ba73831d1da6371d4e121be96c93c
pakchoice <14540728+pakchoice6@user.noreply.gitee.com>
2024-06-17 15:29:18 +0800

    去掉视觉的内部分组

--- const/tmp/innerGroupConst.js
+++ const/tmp/innerGroupConst.js
@@ -43,40 +43,40 @@ const tmInnerGroup = {
 const visionInnerGroup = {
     deptId: "482162119",
     group: [
-        {
-            groupCode: "shGroup", groupName: "施豪组",
-            members: [
-                {userName: "施豪", userDDId: "2257122745843341", isLeader: true},
-                {userName: "张宇", userDDId: "021013633404778343"}
-            ]
-        },
-        {
-            groupCode: "lxyGroup", groupName: "李徐莹组",
-            members: [
-                {userName: "李徐莹", userDDId: "216201066326206711", isLeader: true},
-                {userName: "易敏", userDDId: "023231184344835996"}
-            ]
-        },
-        {
-            groupCode: "zykGroup", groupName: "张月坤组",
-            members: [
-                {userName: "张月坤", userDDId: "045820505124242300", isLeader: true},
-                {userName: "张周", userDDId: "023223231211776520"}
-            ]
-        },
-        {
-            groupCode: "dfGroup", groupName: "丁芳组",
-            members: [{userName: "丁芳", userDDId: "013017535501652498", isLeader: true}]
-        },
-        {
-            groupCode: "syGroup", groupName: "摄影组",
-            members: [
-                {userName: "于浩", userDDId: "2103600332651419"},
-                {userName: "杨雪", userDDId: "1228451750859266"},
-                {userName: "高悦斌", userDDId: "02421711126038887038"},
-                {userName: "芦明阳", userDDId: "02332348011532989963"}
-            ]
-        }
+        // {
+        //     groupCode: "shGroup", groupName: "施豪组",
+        //     members: [
+        //         {userName: "施豪", userDDId: "2257122745843341", isLeader: true},
+        //         {userName: "张宇", userDDId: "021013633404778343"}
+        //     ]
+        // },
+        // {
+        //     groupCode: "lxyGroup", groupName: "李徐莹组",
+        //     members: [
+        //         {userName: "李徐莹", userDDId: "216201066326206711", isLeader: true},
+        //         {userName: "易敏", userDDId: "023231184344835996"}
+        //     ]
+        // },
+        // {
+        //     groupCode: "zykGroup", groupName: "张月坤组",
+        //     members: [
+        //         {userName: "张月坤", userDDId: "045820505124242300", isLeader: true},
+        //         {userName: "张周", userDDId: "023223231211776520"}
+        //     ]
+        // },
+        // {
+        //     groupCode: "dfGroup", groupName: "丁芳组",
+        //     members: [{userName: "丁芳", userDDId: "013017535501652498", isLeader: true}]
+        // },
+        // {
+        //     groupCode: "syGroup", groupName: "摄影组",
+        //     members: [
+        //         {userName: "于浩", userDDId: "2103600332651419"},
+        //         {userName: "杨雪", userDDId: "1228451750859266"},
+        //         {userName: "高悦斌", userDDId: "02421711126038887038"},
+        //         {userName: "芦明阳", userDDId: "02332348011532989963"}
+        //     ]
+        // }
     ]
 }
 
