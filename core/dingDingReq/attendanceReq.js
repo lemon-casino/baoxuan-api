2c59b0f9e8ed1a3a14a41ea75248eecc86b7d153
pakchoice <14540728+pakchoice6@user.noreply.gitee.com>
2024-06-25 11:05:03 +0800

    获取attendances

--- core/dingDingReq/attendanceReq.js
+++ core/dingDingReq/attendanceReq.js
@@ -45,58 +45,7 @@ const getAttendances = async (workDateFrom, workDateTo, userIds, token) => {
     let hasMore = true
     let attendances = []
     while (hasMore) {
-        const result = {
-            "errcode": 0,
-            "hasMore": false,
-            "errmsg": "ok",
-            "recordresult": [
-                {
-                    "checkType": "OffDuty",
-                    "corpId": "dingf5e887e4d232b1b835c2f4657eb6378f",
-                    "locationResult": "Normal",
-                    "baseCheckTime": 1719199800000,
-                    "groupId": 518395063,
-                    "timeResult": "Normal",
-                    "userId": "156133016423090865",
-                    "recordId": 159882794617,
-                    "workDate": 1719158400000,
-                    "sourceType": "ATM",
-                    "userCheckTime": 1719199872000,
-                    "planId": 679524338008,
-                    "id": 335080541886
-                },
-                {
-                    "checkType": "OnDuty",
-                    "corpId": "dingf5e887e4d232b1b835c2f4657eb6378f",
-                    "locationResult": "Normal",
-                    "baseCheckTime": 1719203400000,
-                    "groupId": 518395063,
-                    "timeResult": "Normal",
-                    "userId": "156133016423090865",
-                    "recordId": 159376385918,
-                    "workDate": 1719158400000,
-                    "sourceType": "ATM",
-                    "userCheckTime": 1719201879000,
-                    "planId": 679524338009,
-                    "id": 335087763842
-                },
-                {
-                    "checkType": "OnDuty",
-                    "corpId": "dingf5e887e4d232b1b835c2f4657eb6378f",
-                    "locationResult": "Normal",
-                    "baseCheckTime": 1719190800000,
-                    "groupId": 518395063,
-                    "timeResult": "Normal",
-                    "userId": "156133016423090865",
-                    "recordId": 160354215851,
-                    "workDate": 1719158400000,
-                    "sourceType": "ATM",
-                    "userCheckTime": 1719190408000,
-                    "planId": 679524338007,
-                    "id": 336262866118
-                }
-            ]
-        }//await getPagingAttendances(pageIndex, workDateFrom, workDateTo, userIds, token)
+        const result = await getPagingAttendances(pageIndex, workDateFrom, workDateTo, userIds, token)
         if (result.errmsg !== "ok") {
             throw new RemoteError(errmsg)
         }
