04b3816d20b5ca8c762d9c038af23fbf74d9fa3e
pakchoice <14540728+pakchoice6@user.noreply.gitee.com>
2024-06-28 14:10:26 +0800

    拆分rule

---
+++ model/deptCoreActionFormDetailsRuleModel.js
@@ -0,0 +1,59 @@
+const {
+  DataTypes
+} = require('sequelize');
+module.exports = sequelize => {
+  const attributes = {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: true,
+      autoIncrement: true,
+      comment: null,
+      field: "id"
+    },
+    deptCoreActionFormRuleId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "dept_core_action_form_rule_id"
+    },
+    fieldId: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "field_id"
+    },
+    opCode: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "op_code"
+    },
+    value: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "value"
+    }
+  };
+  const options = {
+    tableName: "dept_core_action_form_details_rule",
+    comment: "",
+    indexes: []
+  };
+  const DeptCoreActionFormDetailsRuleModel = sequelize.define("deptCoreActionFormDetailsRuleModel", attributes, options);
+  return DeptCoreActionFormDetailsRuleModel;
+};
