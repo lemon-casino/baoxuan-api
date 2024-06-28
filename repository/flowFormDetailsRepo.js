9beec7c57f8b2f1a797d90701f0a953044e7fb75
pakchoice <14540728+pakchoice6@user.noreply.gitee.com>
2024-06-28 12:39:11 +0800

    添加字段opCode和value

--- repository/flowFormDetailsRepo.js
+++ repository/flowFormDetailsRepo.js
@@ -21,18 +21,21 @@ const getFormDifferentVersionsDetails = async (formId) => {
             formId
         }
     })
-
+    formDetails = sequelizeUtil.extractDataValues(formDetails)
     formDetails = formDetails.sort((curr, next) => next.id - curr.id)
 
     // todo： deptCoreActionId获取在表单中已经设置的数据，补充进去
+    for (const formDetail of formDetails) {
+        formDetail.opCode = ""
+        formDetail.value = []
+    }
 
     const differentVersionsDetails = []
     while (formDetails.length > 0) {
         const version = formDetails[0].version
         let sameVersionDetails = formDetails.filter(item => item.version === version)
         sameVersionDetails = sameVersionDetails.sort((curr, next) => curr.id - next.id)
-        sameVersionDetails.opCode = ""
-        sameVersionDetails.value = []
+
         differentVersionsDetails.push({title: `表单版本${version}`, details: sameVersionDetails})
 
         const hasCountedIds = sameVersionDetails.map(item => item.id)
