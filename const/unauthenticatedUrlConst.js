8172fe7b9173cc1af068ac4e702eccb7ba5e1640
pakchoice <14540728+pakchoice6@user.noreply.gitee.com>
2024-06-28 09:36:29 +0800

    暂时放行/form-reviews

--- const/unauthenticatedUrlConst.js
+++ const/unauthenticatedUrlConst.js
@@ -13,5 +13,7 @@ module.exports = [
     "/user/flowpath/getoaallprocess",
     "/user/flowpath/getyidaprocess",
     "/flow/all-overdue-running-flows",
+    "/flow/all-overdue-running-flows",
+    "/form-reviews",
     /^\/download\/.*/,
 ]
