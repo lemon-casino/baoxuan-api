e7dc32a0766802be1acd33169f7efb415df89d0e
pakchoice <14540728+pakchoice6@user.noreply.gitee.com>
2024-07-02 12:17:40 +0800

    添加表单信息丢失补丁

---
+++ patch/outModifyPictureVisionPatch.js
@@ -0,0 +1,97 @@
+module.exports = [
+    {
+        processInstanceId: "ec2f3caf-924b-4ea1-b349-8705022dbb96",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "陈天南"
+    },
+    {
+        processInstanceId: "63ac7251-7435-43b0-90f5-619882c81733",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "蔡有萍"
+    },
+    {
+        processInstanceId: "3b182a59-e5a2-4e6c-ba8c-aa59d7092272",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "谢永强"
+    },
+    {
+        processInstanceId: "cf9847df-90d5-44ae-bab5-8e2efc5e0496",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "芬芬"
+    },
+    {
+        processInstanceId: "0df67bcb-d9b4-4547-b340-a6399bc1e956",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "彭晓琴"
+    },
+    {
+        processInstanceId: "0d37d416-4201-406d-aa32-2fa8d349551e",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "范梦霞"
+    },
+    {
+        processInstanceId: "6920168d-7bb5-4740-a1ff-1705a8bb03c5",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "彭晓琴"
+    },
+    {
+        processInstanceId: "78d09038-d31a-4e1c-b68d-89edab7bfb20",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "陈天南"
+    },
+    {
+        processInstanceId: "97544b5c-8007-499d-b126-0dfc8b0018b1",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "徐彩玉"
+    },
+    {
+        processInstanceId: "0d37d416-4201-406d-aa32-2fa8d349551e",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "范梦霞"
+    },
+    {
+        processInstanceId: "6920168d-7bb5-4740-a1ff-1705a8bb03c5",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "彭晓琴"
+    },
+    {
+        processInstanceId: "78d09038-d31a-4e1c-b68d-89edab7bfb20",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "陈天南"
+    },
+    {
+        processInstanceId: "97544b5c-8007-499d-b126-0dfc8b0018b1",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "徐彩玉"
+    },
+    {
+        processInstanceId: "779b24ff-0f91-447c-a8a3-3e2ccba80092",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "蔡有萍"
+    },
+    {
+        processInstanceId: "eeb413c0-c9b1-400a-b463-6441864209ff",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "周俊腾"
+    },
+    {
+        processInstanceId: "d4606e67-7617-4bac-bbb2-9d3a1b2a43c3",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "陈天南"
+    },
+    {
+        processInstanceId: "7f172b1b-d53c-47f0-81e7-922560548a96",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "周俊腾"
+    },
+    {
+        processInstanceId: "d3d04501-dbd1-461e-8b9f-75964c07a47b",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "范梦霞"
+    },
+    {
+        processInstanceId: "2788d794-7b91-4f9d-9266-22f5b125c753",
+        missingFieldId: "textField_lx48e5gk",
+        fieldValue: "范梦霞"
+    }
+]
