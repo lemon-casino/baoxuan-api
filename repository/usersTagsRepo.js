5d5cf7677e11388dd6db3f43bab299b3a66f1348
pakchoice <14540728+pakchoice6@user.noreply.gitee.com>
2024-07-04 19:52:50 +0800

    标签排序

--- repository/usersTagsRepo.js
+++ repository/usersTagsRepo.js
@@ -2,9 +2,14 @@ const models = require('@/model')
 const usersTagsModel = models.usersTagsModel
 
 const getUsersTags = async (where) => {
-    return (await usersTagsModel.findAll({where}))
+    return (await usersTagsModel.findAll(
+        {
+            where,
+            order: [["tagCode", "asc"]]
+        }))
 }
 
+const saveUserTag = async (data) => {
 const saveUserTag = async (data) => {
     return (await usersTagsModel.create(data))
 }
