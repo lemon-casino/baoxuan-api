c2fed932f871123c7ca34986b887ff9576bd9bdb
pakchoice <14540728+pakchoice6@user.noreply.gitee.com>
2024-07-09 10:26:43 +0800

    tmp

--- const/redisConst.js
+++ const/redisConst.js
@@ -1,8 +1,8 @@
 const redisKeys = {
     "TodayRunningAndFinishedFlows": "flows:today:running_and_finished",
-    "Departments": "bases:departments",
-    "DepartmentsUsers": "bases:departments:users",
-    "Users": "bases:users",
+    "Departments": "bases:new:departments",
+    "DepartmentsUsers": "bases:new:departments:users",
+    "Users": "bases:new:users",
     "DDToken": "bases:dd_token",
     "WorkingDays": "bases:working_days",
     "QRCodes": "qr_codes",
