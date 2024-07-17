9f8f7169c4b8f9f54ce7235725c640969b6fd452
loulx <1875483499@qq.com>
2024-07-17 13:21:17 +0800

    优化方案统计采购优化方案流程（全平台）配置调整

--- const/tmp/tmCoreActionsConst/optimizeSchemaActionConst.js
+++ const/tmp/tmCoreActionsConst/optimizeSchemaActionConst.js
@@ -153,13 +153,13 @@ module.exports = [
                             },
                             {
                                 from: {
-                                    id: "node_oclto07a599",
-                                    name: "组长审核方案",
+                                    id: "node_oclyim4za81",
+                                    name: "运营分析五维统计表",
                                     status: ["TODO"]
                                 },
                                 to: {
-                                    id: "node_oclto07a599",
-                                    name: "组长审核方案",
+                                    id: "node_oclyim4za81",
+                                    name: "运营分析五维统计表",
                                     status: ["TODO"]
                                 },
                                 ownerRule: {
@@ -168,23 +168,40 @@ module.exports = [
                                     id: "employeeField_liihs7l0"
                                 }
                             },
-                            {
-                                from: {
-                                    id: "node_oclv7ila163",
-                                    name: "组长审核方案",
-                                    status: ["TODO"]
-                                },
-                                to: {
-                                    id: "node_oclv7ila163",
-                                    name: "组长审核方案",
-                                    status: ["TODO"]
-                                },
-                                ownerRule: {
-                                    from: "form",
-                                    name: "运营负责人",
-                                    id: "employeeField_liihs7l0"
-                                }
-                            }
+                            // {
+                            //     from: {
+                            //         id: "node_oclto07a599",
+                            //         name: "组长审核方案",
+                            //         status: ["TODO"]
+                            //     },
+                            //     to: {
+                            //         id: "node_oclto07a599",
+                            //         name: "组长审核方案",
+                            //         status: ["TODO"]
+                            //     },
+                            //     ownerRule: {
+                            //         from: "form",
+                            //         name: "运营负责人",
+                            //         id: "employeeField_liihs7l0"
+                            //     }
+                            // },
+                            // {
+                            //     from: {
+                            //         id: "node_oclv7ila163",
+                            //         name: "组长审核方案",
+                            //         status: ["TODO"]
+                            //     },
+                            //     to: {
+                            //         id: "node_oclv7ila163",
+                            //         name: "组长审核方案",
+                            //         status: ["TODO"]
+                            //     },
+                            //     ownerRule: {
+                            //         from: "form",
+                            //         name: "运营负责人",
+                            //         id: "employeeField_liihs7l0"
+                            //     }
+                            // }
                         ]
                     }
                 ]
@@ -197,18 +214,19 @@ module.exports = [
                         formName: "运营优化方案流程（全平台）",
                         formId: "FORM-51A6DCCF660B4C1680135461E762AC82JV53",
                         flowDetailsRules: [
-                            {fieldId: "selectField_lk0jfy7h", opCode: opCodes.Equal, value: "简单"}
+                            // 统计表单中selectField_lk0jfy7h为简单或 历史表单中没有该字段的流程
+                            {fieldId: "selectField_lk0jfy7h", opCode: opCodes.NotEqual, value: "困难"}
                         ],
                         flowNodeRules: [
                             {
                                 from: {
-                                    id: "node_oclto07a599",
-                                    name: "组长审核方案",
+                                    id: "node_oclx30r79v2",
+                                    name: "提交优化方案",
                                     status: ["HISTORY"]
                                 },
                                 to: {
-                                    id: "node_oclto07a599",
-                                    name: "组长审核方案",
+                                    id: "node_oclx30r79v2",
+                                    name: "提交优化方案",
                                     status: ["HISTORY"]
                                 },
                                 ownerRule: {
@@ -217,23 +235,40 @@ module.exports = [
                                     id: "employeeField_liihs7l0"
                                 }
                             },
-                            {
-                                from: {
-                                    id: "node_oclv7ila163",
-                                    name: "组长审核方案",
-                                    status: ["HISTORY"]
-                                },
-                                to: {
-                                    id: "node_oclv7ila163",
-                                    name: "组长审核方案",
-                                    status: ["HISTORY"]
-                                },
-                                ownerRule: {
-                                    from: "form",
-                                    name: "运营负责人",
-                                    id: "employeeField_liihs7l0"
-                                }
-                            }
+                            // {
+                            //     from: {
+                            //         id: "node_oclto07a599",
+                            //         name: "组长审核方案",
+                            //         status: ["HISTORY"]
+                            //     },
+                            //     to: {
+                            //         id: "node_oclto07a599",
+                            //         name: "组长审核方案",
+                            //         status: ["HISTORY"]
+                            //     },
+                            //     ownerRule: {
+                            //         from: "form",
+                            //         name: "运营负责人",
+                            //         id: "employeeField_liihs7l0"
+                            //     }
+                            // },
+                            // {
+                            //     from: {
+                            //         id: "node_oclv7ila163",
+                            //         name: "组长审核方案",
+                            //         status: ["HISTORY"]
+                            //     },
+                            //     to: {
+                            //         id: "node_oclv7ila163",
+                            //         name: "组长审核方案",
+                            //         status: ["HISTORY"]
+                            //     },
+                            //     ownerRule: {
+                            //         from: "form",
+                            //         name: "运营负责人",
+                            //         id: "employeeField_liihs7l0"
+                            //     }
+                            // }
                         ]
                     }
                 ]
@@ -391,13 +426,13 @@ module.exports = [
                             },
                             {
                                 from: {
-                                    id: "node_oclto07a599",
-                                    name: "组长审核方案",
+                                    id: "node_oclyim4za81",
+                                    name: "运营分析五维统计表",
                                     status: ["TODO"]
                                 },
                                 to: {
-                                    id: "node_oclto07a599",
-                                    name: "组长审核方案",
+                                    id: "node_oclyim4za81",
+                                    name: "运营分析五维统计表",
                                     status: ["TODO"]
                                 },
                                 ownerRule: {
@@ -406,23 +441,40 @@ module.exports = [
                                     id: "employeeField_liihs7l0"
                                 }
                             },
-                            {
-                                from: {
-                                    id: "node_oclv7ila163",
-                                    name: "组长审核方案",
-                                    status: ["TODO"]
-                                },
-                                to: {
-                                    id: "node_oclv7ila163",
-                                    name: "组长审核方案",
-                                    status: ["TODO"]
-                                },
-                                ownerRule: {
-                                    from: "form",
-                                    name: "运营负责人",
-                                    id: "employeeField_liihs7l0"
-                                }
-                            }
+                            // {
+                            //     from: {
+                            //         id: "node_oclto07a599",
+                            //         name: "组长审核方案",
+                            //         status: ["TODO"]
+                            //     },
+                            //     to: {
+                            //         id: "node_oclto07a599",
+                            //         name: "组长审核方案",
+                            //         status: ["TODO"]
+                            //     },
+                            //     ownerRule: {
+                            //         from: "form",
+                            //         name: "运营负责人",
+                            //         id: "employeeField_liihs7l0"
+                            //     }
+                            // },
+                            // {
+                            //     from: {
+                            //         id: "node_oclv7ila163",
+                            //         name: "组长审核方案",
+                            //         status: ["TODO"]
+                            //     },
+                            //     to: {
+                            //         id: "node_oclv7ila163",
+                            //         name: "组长审核方案",
+                            //         status: ["TODO"]
+                            //     },
+                            //     ownerRule: {
+                            //         from: "form",
+                            //         name: "运营负责人",
+                            //         id: "employeeField_liihs7l0"
+                            //     }
+                            // }
                         ]
                     }
                 ]
@@ -440,13 +492,13 @@ module.exports = [
                         flowNodeRules: [
                             {
                                 from: {
-                                    id: "node_oclto07a599",
-                                    name: "组长审核方案",
+                                    id: "node_oclx30r79v2",
+                                    name: "提交优化方案",
                                     status: ["HISTORY"]
                                 },
                                 to: {
-                                    id: "node_oclto07a599",
-                                    name: "组长审核方案",
+                                    id: "node_oclx30r79v2",
+                                    name: "提交优化方案",
                                     status: ["HISTORY"]
                                 },
                                 ownerRule: {
@@ -455,23 +507,40 @@ module.exports = [
                                     id: "employeeField_liihs7l0"
                                 }
                             },
-                            {
-                                from: {
-                                    id: "node_oclv7ila163",
-                                    name: "组长审核方案",
-                                    status: ["HISTORY"]
-                                },
-                                to: {
-                                    id: "node_oclv7ila163",
-                                    name: "组长审核方案",
-                                    status: ["HISTORY"]
-                                },
-                                ownerRule: {
-                                    from: "form",
-                                    name: "运营负责人",
-                                    id: "employeeField_liihs7l0"
-                                }
-                            }
+                            // {
+                            //     from: {
+                            //         id: "node_oclto07a599",
+                            //         name: "组长审核方案",
+                            //         status: ["HISTORY"]
+                            //     },
+                            //     to: {
+                            //         id: "node_oclto07a599",
+                            //         name: "组长审核方案",
+                            //         status: ["HISTORY"]
+                            //     },
+                            //     ownerRule: {
+                            //         from: "form",
+                            //         name: "运营负责人",
+                            //         id: "employeeField_liihs7l0"
+                            //     }
+                            // },
+                            // {
+                            //     from: {
+                            //         id: "node_oclv7ila163",
+                            //         name: "组长审核方案",
+                            //         status: ["HISTORY"]
+                            //     },
+                            //     to: {
+                            //         id: "node_oclv7ila163",
+                            //         name: "组长审核方案",
+                            //         status: ["HISTORY"]
+                            //     },
+                            //     ownerRule: {
+                            //         from: "form",
+                            //         name: "运营负责人",
+                            //         id: "employeeField_liihs7l0"
+                            //     }
+                            // }
                         ]
                     }
                 ]
