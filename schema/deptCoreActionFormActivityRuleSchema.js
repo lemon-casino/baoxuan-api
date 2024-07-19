dfb271211f7689fa7ee215c32e45242eb727dc4e
loulx <1875483499@qq.com>
2024-07-19 13:01:17 +0800

    tmp

--- schema/deptCoreActionFormActivityRuleSchema.js
+++ schema/deptCoreActionFormActivityRuleSchema.js
@@ -1,19 +1,21 @@
 const joiUtil = require("@/utils/joiUtil")
+const Joi = require("joi");
 
 const saveParamsSchema = {
     deptCoreActionFormRuleId: joiUtil.commonJoiSchemas.numberRequired,
     activityId: joiUtil.commonJoiSchemas.strRequired,
     status: joiUtil.commonJoiSchemas.strRequired,
-    owner: joiUtil.commonJoiSchemas.objectRequire,
+    owner: Joi.object({
+        from: joiUtil.commonJoiSchemas.strRequired,
+        id: joiUtil.commonJoiSchemas.strRequired
+    }).required(),
     version: joiUtil.commonJoiSchemas.numberRequired
 }
 
 const updateParamsSchema = {
-    id: joiUtil.commonJoiSchemas.numberRequired,
-    ...saveParamsSchema
+    id: joiUtil.commonJoiSchemas.numberRequired, ...saveParamsSchema
 }
 
 module.exports = {
-    saveParamsSchema,
-    updateParamsSchema
+    saveParamsSchema, updateParamsSchema
 }
