30168a2f889986fbac04c5b9c9674865d1729184
Lemon <1599456917@qq.com>
2024-07-22 18:14:05 +0800

    天猫竞品文件上传解析以及下载功能

---
+++ model/tmallCompetitor.js
@@ -0,0 +1,113 @@
+const {
+  DataTypes
+} = require('sequelize');
+module.exports = sequelize => {
+  const attributes = {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: true,
+      autoIncrement: true,
+      comment: null,
+      field: "id"
+    },
+    linkId: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: "链接ID",
+      field: "link_id"
+    },
+    headOfOperations: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: "维护人- 运营负责人",
+      field: "headOf_operations"
+    },
+    headOfProductLine: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: "运营负责人",
+      field: "headOf_productLine"
+    },
+    storeName: {
+      type: DataTypes.STRING(10),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: "店铺名称",
+      field: "store_name"
+    },
+    productName: {
+      type: DataTypes.STRING(10),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: "产品名称",
+      field: "product_name"
+    },
+    competitorId: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: "竞品ID",
+      field: "competitor_id"
+    },
+    category: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: "类别",
+      field: "category"
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: "日期",
+      field: "date"
+    },
+    search: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: "搜索",
+      field: "search"
+    },
+    numberSearches: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: "0",
+      primaryKey: false,
+      autoIncrement: false,
+      comment: "搜索人数",
+      field: "number_searches"
+    }
+  };
+  const options = {
+    tableName: "tmall_competitor",
+    comment: "",
+    indexes: []
+  };
+  const TmallCompetitorModel = sequelize.define("tmallCompetitorModel", attributes, options);
+  return TmallCompetitorModel;
+};
