19e8ecd2955f90fa79c10800f36f6833b7be771c
loulx <1875483499@qq.com>
2024-07-23 19:20:22 +0800

    将数据库相关的配置信息放到env

--- config/prod.js
+++ config/prod.js
@@ -1,27 +1,29 @@
 const defaultConfig = require('./default')
+const nConf = require("nconf")
+nConf.env()
 
 const configs = {
     ...defaultConfig,
-    redisConfig:{
+    redisConfig: {
         port: 6379,
         url: '127.0.0.1',
-        password: 'myredis123456',
+        password: nConf.get("BI_PROD_REDIS_PWD"),
         ddSuiteKey: "",
     },
     dbConfig: {
-        dbName: 'Bi_serve',
-        username: 'bqh',
-        password: 'bqh2024^',
-        host: 'rm-2zeomo30f6062u9r37o.mysql.rds.aliyuncs.com',
-        port: 3306,
+        dbName: nConf.get("BI_PROD_DB_NAME"),
+        username: nConf.get("BI_PROD_DB_USER_NAME"),
+        password: nConf.get("BI_PROD_DB_PWD"),
+        host: nConf.get("BI_PROD_DB_HOST"),
+        port: nConf.get("BI_PROD_DB_PORT"),
         define: {
             timestamps: false
         },
         logging: false,
         timezone: '+08:00'
     },
-    serverConfig:{
-        port: 9999
+    serverConfig: {
+        port: nConf.get("BI_PROD_SERVER_PORT")
     }
 }
 
