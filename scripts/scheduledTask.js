de9fd7c45c989b18229fcbdbbd6780471a8f06e5
Lemon <1599456917@qq.com>
2024-07-26 23:57:55 +0800

    修改到准确触发时间

--- scripts/scheduledTask.js
+++ scripts/scheduledTask.js
@@ -20,7 +20,7 @@ let syncUserLoginCron = "0 0/5 * * * ?"
 let syncResignEmployeeCron = "0 0 18 * * ?"
 let syncRunningFlowsCron = "0 0 8 * * ?"
 let tmallLinkAnomalyDetection = "0 0 23 * * ?"
-let linshi = "42 23 * * *"
+let linshi = "00 14 * * *"
 if (process.env.NODE_ENV === "dev") {
     syncWorkingDayCron = "0 5 10 * * ?"
     syncTodayRunningAndFinishedFlowsCron = "0 10 12 * * ?"
