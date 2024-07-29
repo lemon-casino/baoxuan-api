e1f663ce4909c9411e710bac6557f3e0971c78a1
Lemon <1599456917@qq.com>
2024-07-29 15:11:10 +0800

    上传文件 每周一 以及每个月1号 更新周转化 月转化计算

--- service/tmallCompetitorService.js
+++ service/tmallCompetitorService.js
@@ -18,38 +18,40 @@ const uploadSingleIteTaoBaoCompetitorTable = async (item) => {
     });
     return await tmallCompetitorRepo.uploadSingleIteTaoBaoCompetitorTable(item);
 }
-const searchSingleIteTaoBaoCompetitorTable = async (search) => {
-    return await tmallCompetitorRepo.searchSingleIteTaoBaoCompetitorTable(search);
+const searchSingleIteTaoBaoCompetitorTable = async (searchParams, page, pageSize) => {
+    return await tmallCompetitorRepo.searchSingleIteTaoBaoCompetitorTable(searchParams, page, pageSize);
 }
+const addItemToResult = (item, key, result) => {
+    if (item[key] && !result[key].some(e => e.value === item[key])) {
+        result[key].push({ value: item[key], label: item[key] });
+    }
+};
+
 const conditionalFiltering = async (result) => {
     let data = await tmallCompetitorRepo.conditionalFiltering();
+    const keys = [
+        'link_id',
+        'competitors_id',
+        'headOf_operations',
+        'competitors_name',
+        'category',
+        'headOf_productLine',
+        'store_name',
+        'month_on_month',
+        'week_on_week'
+    ];
+
     data.forEach(item => {
-        if (item.link_id && !result.link_id.some(e => e.value === item.link_id)) {
-            result.link_id.push({ value: item.link_id, label: item.link_id });
-        }
-        if (item.competitor_id && !result.competitors_id.some(e => e.value === item.competitor_id)) {
-            result.competitors_id.push({ value: item.competitor_id, label: item.competitor_id });
-        }
-        if (item.headOf_operations && !result.headOf_operations.some(e => e.value === item.headOf_operations)) {
-            result.headOf_operations.push({ value: item.headOf_operations, label: item.headOf_operations });
-        }
-        if (item.competitors_name && !result.competitors_name.some(e => e.value === item.competitors_name)) {
-            result.competitors_name.push({ value: item.competitors_name, label: item.competitors_name });
-        }
-        if (item.category && !result.category.some(e => e.value === item.category)) {
-            result.category.push({ value: item.category, label: item.category });
-        }
-        if (item.headOf_productLine && !result.headOf_productLine.some(e => e.value === item.headOf_productLine)) {
-            result.headOf_productLine.push({ value: item.headOf_productLine, label: item.headOf_productLine });
-        }
-        if (item.store_name && !result.store_name.some(e => e.value === item.store_name)) {
-            result.store_name.push({ value: item.store_name, label: item.store_name });
-        }
-
+        keys.forEach(key => {
+            addItemToResult(item, key, result);
+        });
     });
-
     return result;
 }
+
+
+
+
 module.exports = {
 
     uploadSingleIteTaoBaoCompetitorTable,
