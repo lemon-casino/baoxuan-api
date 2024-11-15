35f18dca64277944182c2655c0c8d33c5694d59b
lemon <1599456917@qq.com>
2024-11-15 14:13:20 +0800

    随机延迟

--- scripts/scheduledTask.js
+++ scripts/scheduledTask.js
@@ -21,7 +21,7 @@ let syncUserLoginCron = "0 0/5 * * * ?"
 let syncResignEmployeeCron = "0 0 18 * * ?"
 let syncRunningFlowsCron = "0 0 8 * * ?"
 let tmallLinkData = "32 14 * * 1-6"
-let jdLinkData  = "31 13 * * 1-6"
+let jdLinkData  = "17 14 * * 1-6"
 let caigouLinkData  = "*/5 * * * 1-6"
 //转正通知 周一到周六  每天9点半触发流程
 let confirmationNotice = "0 30 9 * * 1-6"
