1f4179f26d236e4ef55dcc7704e0582ace36f9e5
zjzjch <14099347+zjzjch@user.noreply.gitee.com>
2024-12-13 18:42:39 +0800

    tianmao

--- service/dianShangOperationAttributeService.js
+++ service/dianShangOperationAttributeService.js
@@ -70,6 +70,39 @@ const uploadBulkUploadsTable = async (translatedData) => {
     }
 
 };
+const uploadtmBulkUploadsTable = async (translatedData) => {
+    // 提取 translatedData 中的 goodsId，并使用 Map 关联 goodsId 与对应的对象
+    const translatedDataMap = new Map(translatedData.map(item => [item.goodsId, item]));
+    console.log(translatedDataMap)
+    // 获取数据库中的所有 goodsId，并转换为 Set 以便于快速查找
+    const dbData = await dianShangOperationAttributeRepo.getAllGoodsAttrDetails();
+    const dbDataSet = new Set(dbData);
+    console.log(dbDataSet)
+    // 分类数据，区分为需要更新和需要插入的数据
+    const updates = [];
+    const inserts = [];
+
+    for (const [goodsId, obj] of translatedDataMap.entries()) {
+        if (dbDataSet.has(goodsId)) {
+            updates.push(obj);  // 如果 goodsId 存在，放入更新列表
+        } else {
+            inserts.push(obj);  // 如果 goodsId 不存在，放入插入列表
+        }
+    }
+
+    // 执行更新操作
+
+    if (updates.length > 0) {
+        await dianShangOperationAttributeRepo.updategoodsIdAttrDetails(updates);
+    }
+    // 执行插入操作
+    //如何inserts 不为空
+    if (inserts.length > 0) {
+        // 执行批量插入操作
+        await dianShangOperationAttributeRepo.bulkCreateTable(inserts);
+    }
+
+};
 
 module.exports = {
     getPagingOperateAttributes,
@@ -77,5 +110,6 @@ module.exports = {
     updateProductAttrDetails,
     saveProductAttr,
     deleteProductAttr,
-    uploadBulkUploadsTable
+    uploadBulkUploadsTable,
+    uploadtmBulkUploadsTable
 }
