eb299106d6976fa839aab7f4a1cb667293670170
zjzjch <14099347+zjzjch@user.noreply.gitee.com>
2025-06-25 19:00:30 +0800

    01

--- repository/operation/goodsSaleVerifiedRepo.js
+++ repository/operation/goodsSaleVerifiedRepo.js
@@ -888,8 +888,14 @@ goodsSaleVerifiedRepo.updateOrder = async ({
 }
 
 goodsSaleVerifiedRepo.deleteByDate = async (date) => {
-    let sql = `DELETE FROM goods_sale_verified WHERE \`date\` = ?`
+    let sql = `DELETE FROM goods_sale_verified WHERE \`date\` = ? and goods_id not in ('123456789','123456781')`
     const result = await query(sql, [date])
+    return result?.affectedRows ? true : false
+}
+
+goodsSaleVerifiedRepo.deleteByDateId = async (date,goods_id) => {
+    let sql = `DELETE FROM goods_sale_verified WHERE \`date\` = ? and goods_id = ?`
+    const result = await query(sql, [date,goods_id])
     return result?.affectedRows ? true : false
 }
 
