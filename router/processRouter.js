d4d06a51b6ec0342a77b91c868febe5a91475088
lareinalcc <lareinalcc@qq.com>
2025-10-27 13:58:23 +0800

    update

--- router/processRouter.js
+++ router/processRouter.js
@@ -10,4 +10,9 @@ router.post("/getProcessIdsTmpData",processApi.getProcessIdsTmpData)
 // 查询进行中和和已完成表数据
 router.post("/getProcessMergeIdsData",processApi.getProcessMergeIdsData)
 
+//推品全流程统计
+router.get('/development/total', processApi.getDevelopmentProcessTotal)
+router.get('/development/list', processApi.getDevelopmentProcessList)
+router.post('/development/create', processApi.createDevelopmentProcess)
+
 module.exports = router
