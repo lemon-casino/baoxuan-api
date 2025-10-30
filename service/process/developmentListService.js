aae7c6c72f58c8aea501d1a1458b7dc0e72f498f
lemon-casino <ningmengguorou@gmail.com>
2025-10-30 17:21:15 +0800

    Support operator inquiry detail lists

--- service/process/developmentListService.js
+++ service/process/developmentListService.js
@@ -10,12 +10,25 @@ const FIELD_TO_TYPE_MAP = {
     self: typeList.SELF
 }
 
+const INQUIRY_FIELD_TO_STATUS = {
+    inquiry_running: 'running',
+    inquiry_success: 'success',
+    inquiry_fail: 'fail'
+}
+
 /**
  * 根据字段名解析对应的推品类型
  * @param {string} field 前端传入的字段标识
  * @returns {string|undefined} 对应的推品类型
  */
 const resolveDevelopmentType = (field) => FIELD_TO_TYPE_MAP[field]
+
+/**
+ * 根据字段名解析反推询价的状态
+ * @param {string} field 前端传入的字段标识
+ * @returns {string|undefined} 对应的状态
+ */
+const resolveInquiryStatus = (field) => INQUIRY_FIELD_TO_STATUS[field]
 
 /**
  * 查询满足条件的推品明细列表
@@ -36,6 +49,24 @@ const queryDevelopmentList = async (isRunningMode, developmentType, startDate, e
 }
 
 /**
+ * 查询反推询价对应状态的推品明细
+ * @param {boolean} isRunningMode 是否为待办模式
+ * @param {string} status 目标状态
+ * @param {string|undefined} startDate 开始日期
+ * @param {string|undefined} endDate 结束日期
+ * @returns {Promise<Array<object>>} 推品明细列表
+ */
+const queryInquiryList = async (isRunningMode, status, startDate, endDate) => {
+    const options = {
+        status,
+        isRunningMode,
+        start: isRunningMode ? undefined : startDate,
+        end: isRunningMode ? undefined : endDate
+    }
+    return processesRepo.getOperatorInquiryList(options)
+}
+
+/**
  * 获取推品全流程的明细数据
  * @param {string} type 查询模式，0 表示发起模式，1 表示待办模式
  * @param {string} field 前端点击的字段标识
@@ -45,13 +76,18 @@ const queryDevelopmentList = async (isRunningMode, developmentType, startDate, e
  */
 const getDevelopmentProcessList = async (type, field, startDate, endDate) => {
     const columns = defaultColumns.map((column) => ({ ...column }))
-    const developmentType = resolveDevelopmentType(field)
-    if (!developmentType) {
-        return { columns, data: [] }
-    }
     const isRunningMode = type === '1'
-    const data = await queryDevelopmentList(isRunningMode, developmentType, startDate, endDate)
-    return { columns, data }
+    const developmentType = resolveDevelopmentType(field)
+    if (developmentType) {
+        const data = await queryDevelopmentList(isRunningMode, developmentType, startDate, endDate)
+        return { columns, data }
+    }
+    const inquiryStatus = resolveInquiryStatus(field)
+    if (inquiryStatus) {
+        const data = await queryInquiryList(isRunningMode, inquiryStatus, startDate, endDate)
+        return { columns, data }
+    }
+    return { columns, data: [] }
 }
 
 module.exports = {
